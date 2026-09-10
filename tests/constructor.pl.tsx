9e329cc34c72104581f5e5b66faf419a0e9d7e9c
Irina Fominykh <irina.fmn96@yandex.ru>
2026-09-10 16:11:37 +0400

    тест добавление ингредиента

--- tests/constructor.pl.tsx
+++ tests/constructor.pl.tsx
@@ -1,12 +1,25 @@
 import { test, expect } from '@playwright/test';
 
-test('должен записать HAR-файл', async ({ page }) => {
+test('открывается конструктор', async ({ page }) => {
   await page.routeFromHAR('./tests/hars/ingredients.har', {
-    url: '**/ingredients',
-    update: false
+    url: '**/ingredients'
+  });
+  await page.goto('/');
+  await expect(page.getByText('Соберите бургер')).toBeVisible();
+});
+test('добавление ингредиента', async ({ page }) => {
+  await page.routeFromHAR('./tests/hars/ingredients.har', {
+    url: '**/ingredients'
   });
 
   await page.goto('/');
 
-  await expect(page.getByText('Соберите бургер')).toBeVisible();
+  const ingredient = page
+    .locator('li')
+    .filter({ hasText: 'Краторная булка N-200i' });
+
+  await ingredient.getByRole('button', { name: 'Добавить' }).click();
+
+  await expect(page.getByText('Краторная булка N-200i (верх)'));
+  await expect(page.getByText('Краторная булка N-200i (низ)')).toBeVisible();
 });
