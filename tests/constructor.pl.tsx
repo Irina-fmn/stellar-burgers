f89ff72c88fcc488bd6a06e300d146fd6dd38335
Irina Fominykh <irina.fmn96@yandex.ru>
2026-09-10 16:11:37 +0400

    тест добавление  ингридиента - начинка

--- tests/constructor.pl.tsx
+++ tests/constructor.pl.tsx
@@ -1,25 +1,31 @@
 import { test, expect } from '@playwright/test';
 
-test('открывается конструктор', async ({ page }) => {
-  await page.routeFromHAR('./tests/hars/ingredients.har', {
-    url: '**/ingredients'
-  });
-  await page.goto('/');
-  await expect(page.getByText('Соберите бургер')).toBeVisible();
-});
-test('добавление ингредиента', async ({ page }) => {
+test('добавление ингредиентов в конструктор', async ({ page }) => {
   await page.routeFromHAR('./tests/hars/ingredients.har', {
     url: '**/ingredients'
   });
 
   await page.goto('/');
 
-  const ingredient = page
-    .locator('li')
-    .filter({ hasText: 'Краторная булка N-200i' });
+  // Добавляем булку
+  const bun = page.locator('li').filter({ hasText: 'Краторная булка N-200i' });
 
-  await ingredient.getByRole('button', { name: 'Добавить' }).click();
+  await bun.getByRole('button', { name: 'Добавить' }).click();
 
-  await expect(page.getByText('Краторная булка N-200i (верх)'));
+  await expect(page.getByText('Краторная булка N-200i (верх)')).toBeVisible();
+
   await expect(page.getByText('Краторная булка N-200i (низ)')).toBeVisible();
+
+  // Добавляем начинку
+  const filling = page
+    .locator('li')
+    .filter({ hasText: 'Биокотлета из марсианской Магнолии' });
+
+  await filling.getByRole('button', { name: 'Добавить' }).click();
+
+  await expect(
+    page.locator('.constructor-element__text').filter({
+      hasText: 'Биокотлета из марсианской Магнолии'
+    })
+  ).toBeVisible();
 });
